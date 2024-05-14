cad86a9446e08071b0155baa8afc41778958fe2a
Edward xc <3313338649@qq.com>
2024-05-14 19:59:54 +0800

    Intial

---
+++ C/1-TwoSum.c
@@ -0,0 +1,12 @@
+//
+// Created by 33133 on 24-4-26.
+//
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+
+}
